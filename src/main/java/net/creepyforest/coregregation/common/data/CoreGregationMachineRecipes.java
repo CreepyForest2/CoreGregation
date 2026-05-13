14f07ad7050695174c2d9041566cfa7acb4c19bc
CreepyForest2 <davudghiur2@gmail.com>
2026-05-13 07:31:21 +0300

    removed the test recipes

--- src/main/java/net/creepyforest/coregregation/common/data/CoreGregationMachineRecipes.java
+++ src/main/java/net/creepyforest/coregregation/common/data/CoreGregationMachineRecipes.java
@@ -12,21 +12,5 @@ import static com.gregtechceu.gtceu.common.data.GTItems.*;
 
 public class CoreGregationMachineRecipes {
     public static void init(Consumer<FinishedRecipe> provider) {
-
-        CoreGregationRecipeTypes.OVEN_RECIPES.recipeBuilder("testoven")
-                .inputItems(GTItems.DOUGH, 1)
-                .outputItems(ELECTRONIC_CIRCUIT_MV)
-                .save(provider);
-
-        CoreGregationRecipeTypes.COOKING_POT_RECIPES.recipeBuilder("testpot")
-                .inputItems(GTItems.CAPACITOR, 1)
-                .inputFluids(GTMaterials.Deuterium.getFluid(375))
-                .outputItems(ELECTRONIC_CIRCUIT_MV)
-                .save(provider);
-
-        CoreGregationRecipeTypes.BLOOMERY_RECIPES.recipeBuilder("testbloomery")
-                .inputItems(GTItems.SMD_DIODE, 1)
-                .outputItems(NEUTRONIUM_WAFER)
-                .save(provider);
     }
 }
